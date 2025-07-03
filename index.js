f796ddaf21c6085d61257ab4b5e44cb519d7a6e7
Rodrigo Delduca <rodrigo@delduca.org>
2025-07-03 15:05:48 -0300

    Work in progress

--- index.js
+++ index.js
@@ -1,12 +1,49 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, protocol } = require('electron')
+const fs = require('fs/promises')
+const path = require('path')
+const { watch } = require('fs')
 
-const createWindow = () => {
-  const window = new BrowserWindow({
-    width: 800,
-    height: 600
-  })
+const ROOT = __dirname
+const WASM_PATH = path.join(ROOT, '../carimbo/build/carimbo.wasm')
+
+const data = {
+  runtime: null,
+  bundle: null,
 }
 
-app.whenReady().then(() => {
-  createWindow()
+let mainWindow = null
+
+app.whenReady().then(async () => {
+  data.runtime = await fs.readFile(WASM_PATH)
+
+  protocol.interceptBufferProtocol('file', async (request, callback) => {
+    const pathname = decodeURIComponent(new URL(request.url).pathname)
+
+    if (pathname.endsWith('/carimbo.wasm')) {
+      callback({ mimeType: 'application/wasm', data: data.runtime })
+      return
+    }
+
+    callback({
+      mimeType: getMime(pathname),
+      data: await fs.readFile(pathname)
+    })
+  })
+
+  mainWindow = new BrowserWindow({ width: 800, height: 600 })
+  mainWindow.loadURL(`file://${ROOT}/index.html`)
+
+  watch(WASM_PATH, { persistent: false }, () => {
+    mainWindow.webContents.reloadIgnoringCache()
+  })
 })
+
+function getMime(file) {
+  switch (true) {
+    case file.endsWith('.html'): return 'text/html'
+    case file.endsWith('.js'): return 'application/javascript'
+    case file.endsWith('.wasm'): return 'application/wasm'
+    case file.endsWith('.7z'): return 'application/x-7z-compressed'
+  }
+  return 'application/octet-stream'
+}
